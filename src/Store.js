fce903da5f30086ae1e23790d3870b4f515f8b9d
unknown <bagrat.injgia24@gmail.com>
2023-11-10 13:10:28 +0400

    Working With Action Creators

--- src/Store.js
+++ src/Store.js
@@ -13,6 +13,7 @@ function reducer(state = initialState, action) {
     case "account/withdraw":
       return { ...state, balance: state.balance - action.payload };
     case "account/loanRequierd":
+      if (state.loan > 0) return state;
       return {
         ...state,
         loan: action.payload.amount,
@@ -25,21 +26,50 @@ function reducer(state = initialState, action) {
         ...state,
         loan: state.loan - action.payload.pay,
         balance: state.balance - action.payload.pay,
+        loanPurpose: "",
       };
   }
 }
 
 const Store = createStore(reducer);
 
-Store.dispatch({ type: "account/deposit", payload: 500 });
-Store.dispatch({ type: "account/withdraw", payload: 100 });
-Store.dispatch({
-  type: "account/loanRequierd",
-  payload: { amount: 1000, purpuse: "buy a car" },
-});
-Store.dispatch({
-  type: "account/loanPay",
-  payload: { pay: 500 },
-});
+// Store.dispatch({ type: "account/deposit", payload: 500 });
+// Store.dispatch({ type: "account/withdraw", payload: 100 });
+// Store.dispatch({
+//   type: "account/loanRequierd",
+//   payload: { amount: 1000, purpuse: "buy a car" },
+// });
+// Store.dispatch({
+//   type: "account/loanPay",
+//   payload: { pay: 500 },
+// });
 
+// console.log(Store.getState());
+
+function deposite(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+function loanRequierd(amount, purpuse) {
+  return {
+    type: "account/loanRequierd",
+    payload: { amount, purpuse },
+  };
+}
+
+function loanPay(pay) {
+  return {
+    type: "account/loanPay",
+    payload: { pay },
+  };
+}
+
+Store.dispatch(deposite(800));
+Store.dispatch(withdraw(400));
+Store.dispatch(loanRequierd(500, "buy a new Home"));
+Store.dispatch(loanPay(100));
 console.log(Store.getState());
